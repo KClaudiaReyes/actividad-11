b066e6b01c855678cb968b6894cc6c6a0e7045ea
clauR <creyesccf@gmail.com>
2026-03-08 01:06:05 +0100

    1

---
+++ 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    internal class Program
+    {
+        static void saludar ()
+        {
+            Console.WriteLine(" HOLA, BIENVENIDO");
+        }
+
+
+        static void Main(string[] args)
+        {
+            saludar();
+        }
+    }
+}
