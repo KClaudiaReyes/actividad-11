a98ac4058dbedc0513fb1cb3efb0c976edbec72d
clauR <creyesccf@gmail.com>
2026-03-09 00:39:55 +0100

    terminacion del ejercicio 21

--- 21/Program.cs
+++ 21/Program.cs
@@ -10,11 +10,28 @@ namespace _21
     {
         static void Main(string[] args)
         {
+            string text = "ingenieria ";
+            int TotalVoacal= ContarVocales(text);
+
+            Console.WriteLine("la palabra: "+text+" tiene " +TotalVoacal+" vocales en total ");
+
         }
 
         static int ContarVocales(string texto) 
         {
-            
+         int cont = 0;
+            string vocal = texto.ToLower();
+            for (int i = 0; i < vocal.Length; i++)
+            {
+                char letra= vocal[i];
+
+                if (letra == 'a' || letra == 'e' || letra == 'i' || letra== 'o'|| letra == 'u')
+                {
+                    cont++;
+                }
+
+            }
+            return cont;
         }
 
     }
