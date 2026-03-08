0d861e89abbfb993ae31714c615f8d5c254344ce
clauR <creyesccf@gmail.com>
2026-03-08 22:02:45 +0100

    ejrcicio 11 sumar

---
+++ 11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace _11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sumar(1995,30);
+        }
+
+        static void sumar(int a,int b)
+        {
+            int suma = a + b;
+            Console.WriteLine("el resultado es: "+suma);
+        }
+    }
+}
