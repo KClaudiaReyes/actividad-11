edd6f8069e367d269f46d9b978756945afc0e41c
clauR <creyesccf@gmail.com>
2026-03-08 01:39:09 +0100

    ejrcicios 1 al 5

---
+++ 2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Saludar();
+            MostrarNombre();
+            MostrarMensaje();
+            Linea();
+            MostrarMenu();
+        }
+
+        static void Saludar ()
+        {
+            Console.WriteLine(" HOLA , BIENVENIDO ");
+        }
+        static void MostrarNombre()
+        {
+            string NAME = " ALEJANDRA";
+            Console.WriteLine(" HOLA, " + NAME );
+            
+        }
+
+        static void MostrarMensaje()
+        {
+            Console.WriteLine(" Programacion en C# ");
+        }
+
+        static void Linea() 
+        {
+
+            Console.WriteLine("-------------------------");
+        }
+
+        static void MostrarMenu() 
+        {
+
+            Console.WriteLine(" ____MENU______");
+            Console.WriteLine(" 1. ingresar  ");
+            Console.WriteLine(" 2. Mostrar ");
+            Console.WriteLine(" 3. Salir ");
+        }
+
+    }
+}
