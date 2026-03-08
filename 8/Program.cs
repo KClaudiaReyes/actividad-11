617c9804cf96a7822208747129b49df479e32e20
clauR <creyesccf@gmail.com>
2026-03-08 02:11:44 +0100

    ejrcicio 8

---
+++ 8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MostarCuadrado(4);
+        }
+        static void MostarCuadrado(int n)
+        {
+            int cuadrado= n * n;
+            Console.WriteLine(" El cuadrado de "+ n+" es: " +cuadrado);
+
+        }
+    }
+}
