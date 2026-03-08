5a0bc5b9bf317c59670870115dba380652cab033
clauR <creyesccf@gmail.com>
2026-03-08 22:08:50 +0100

    12

---
+++ 12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static void restar(int a, int b)
+        {
+            int resta= a-b;
+        }
+    }
+}
