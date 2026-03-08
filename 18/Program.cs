16df6dca8c985acd0f3b31150d3740c7e5ee51a6
clauR <creyesccf@gmail.com>
2026-03-08 23:40:13 +0100

    18

---
+++ 18/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 27;
+        }
+        static int MenorDeTRes(int a,int b,int c)
+        {
+            if (a < b || a < c)
+            {
+                return a;
+            }
+            else if (b < c)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+        }
+    }
+}
