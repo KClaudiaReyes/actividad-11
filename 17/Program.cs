3a69a50415a0908cc475690320ef325a3411fcb9
clauR <creyesccf@gmail.com>
2026-03-08 23:30:05 +0100

    17

---
+++ 17/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int Mayor(int a ,int b)
+        {
+            
+            if (a > b)
+            {
+                return a;
+            }
+            else 
+            {
+                return b;
+            }
+        }
+    }
+}
