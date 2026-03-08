5e293af553f971d1a4b8d792c87e37338155c283
clauR <creyesccf@gmail.com>
2026-03-09 00:11:26 +0100

    19

---
+++ 19/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("es multiplo?" + EsMultiplo(15,4));
+        }
+
+        static bool EsMultiplo(int n , int m)
+        {
+            if( n % m == 0)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
