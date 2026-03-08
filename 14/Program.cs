208dac938e5526e1cd1f925319da3020bc4952b3
clauR <creyesccf@gmail.com>
2026-03-08 22:42:38 +0100

    ejercicio 14

---
+++ 14/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dividir(90,3);
+        }
+
+        static void Dividir(double a, double b)
+        {
+            double result = a / b;
+            Console.WriteLine(" el resultado es: "+result);
+        }
+    }
+}
