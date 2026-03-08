3069af805617982f9dd6fb06921101b1e667b898
clauR <creyesccf@gmail.com>
2026-03-08 23:22:13 +0100

    ejerrcico 16

---
+++ 16/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" El numero es positivo ? "+EsPositivo(-5));
+        }
+
+        static bool EsPositivo(int n)
+        {
+            if (n > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
