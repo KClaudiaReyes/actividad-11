e6b9637c450afe6566e09e2a777efdbbcc2cb065
clauR <creyesccf@gmail.com>
2026-03-09 00:23:26 +0100

    20

---
+++ 20/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = " programacion";
+        }
+
+        static int ContarLetras(string texto)
+        { 
+            int cont = 0;
+            for (int i = 0; i < texto.Length; i++)
+            {
+                cont++;
+            }
+            return cont;
+        }
+    }
+}
