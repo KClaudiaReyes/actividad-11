080604dbb4233d3901e3670b236a0e4a5dfa46e1
clauR <creyesccf@gmail.com>
2026-03-09 00:42:33 +0100

    22

---
+++ 22/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static int Repetir(string texto,int veces)
+        {
+
+        }
+    }
+}
