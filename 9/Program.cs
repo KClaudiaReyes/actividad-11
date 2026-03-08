9406f3c701cdc80239a8e2b99e5b9fce1748ba11
clauR <creyesccf@gmail.com>
2026-03-08 02:18:51 +0100

    ejrcico 9

---
+++ 9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SumarMostrar(5,25);
+        }
+
+        static void SumarMostrar(int a, int b)
+        {
+            int resultado = a + b;
+            Console.WriteLine("el resultado de sumar A:"+a+ " + " +"B:"+ b+" es: " +resultado);
+        }
+    }
+}
