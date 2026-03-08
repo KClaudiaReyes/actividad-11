c2ff27354be8f504a29fe66afe9d196e3c5e09ca
clauR <creyesccf@gmail.com>
2026-03-08 22:20:58 +0100

    13

---
+++ 13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void Multiplicar(int a, int b)
+        {
+            int multiplicacion = a * b;
+        }
+    }
+}
