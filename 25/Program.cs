3a1285876f1fc18082da309c4d3e278f6772f30f
clauR <creyesccf@gmail.com>
2026-03-09 01:36:07 +0100

    terminacion del 25

--- 25/Program.cs
+++ 25/Program.cs
@@ -10,15 +10,25 @@ namespace _25
     {
         static void Main(string[] args)
         {
+            int num = 7;
+            bool result = EsPrimo(num);
+            Console.WriteLine(result);
         }
 
         static bool EsPrimo(int n)
         {
-            for (int i = 0; i < n; i++)
+            for (int i = 2; i < n; i++)
             {
-                
+                if (n%i ==0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;        
+                }
             }
-
+           return true;
 
         }
     }
