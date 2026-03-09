3cbb4eac2ee1caa0baaacb07d11c2cb466ebf30b
clauR <creyesccf@gmail.com>
2026-03-09 01:12:58 +0100

    terminacion de 23

--- 23/Program.cs
+++ 23/Program.cs
@@ -10,10 +10,19 @@ namespace _23
     {
         static void Main(string[] args)
         {
-        }
-        static int SumarDEl1HastaELN(int n)
-        {
+            int n = 4;
+            int resultado = SumarDel1HastaElN(n);
+            Console.WriteLine("la suma de 1 hasta "+ n +" es: "  +resultado);
             
+        }
+        static int SumarDel1HastaElN(int n)
+        {
+            int suma = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                suma +=i;
+            }
+            return suma;
         }
     }
 }
